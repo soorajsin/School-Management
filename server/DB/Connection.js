63baef9b9ae7961eaf4478112d489b205ba44b8e
Sooraj <soorajsingh7505@gmail.com>
2024-01-03 14:51:08 +0530

    upload

---
+++ server/DB/Connection.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const url = "mongodb+srv://soorajsingh7505:sooraj231@crud-app.4oebebt.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(url).then(() => {
+          console.log("Databse connected");
+}).catch((error) => {
+          console.log(`Error ${error}`);
+});
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 4000 || process.env.port;
+require("./DB/Connection");
 
 
 app.get("/", (req, res) => {
